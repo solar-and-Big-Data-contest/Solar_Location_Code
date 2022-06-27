c86ba189c16499cd5bebd0c85618c90459c83a47
kim geon han <gokimkq111@naver.com>
2022-06-27 14:12:21 +0900

    Update solrad_annual.py

--- Map_annual/solrad_annual.py
+++ Map_annual/solrad_annual.py
@@ -4,10 +4,10 @@ import time
 from tqdm import tqdm
 
 df1 = pd.read_csv('Nrel_alldata.csv',encoding='cp949')
-df1.rename(columns={'solrad_annual':'solrad_annual(kWh/m2/day)'},inplace=True)
+df1.rename(columns={'solrad_annual':'연간 태양 입사량(kWh/m2/day)'},inplace=True)
 px.set_mapbox_access_token(open("mapbox_token.py").read()) # you will need your own token
 
-fig = px.scatter_mapbox(df1, lat="lat", lon="lng", color='solrad_annual(kWh/m2/day)',hover_name="주소",hover_data=["solrad_annual(kWh/m2/day)","노선명"],
+fig = px.scatter_mapbox(df1, lat="lat", lon="lng", color='연간 태양 입사량(kWh/m2/day)',hover_name="주소",hover_data=["연간 태양 입사량(kWh/m2/day)","노선명"],
                         color_continuous_scale=px.colors.cyclical.IceFire,zoom=7
                        )
 
