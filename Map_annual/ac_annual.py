244c405c9880e333ac0d816509b422b569a9d6f3
kim geon han <gokimkq111@naver.com>
2022-06-27 14:12:47 +0900

    Update ac_annual.py

--- Map_annual/ac_annual.py
+++ Map_annual/ac_annual.py
@@ -4,12 +4,12 @@ import time
 from tqdm import tqdm
 
 df2 = pd.read_csv('Nrel_alldata.csv',encoding='cp949')
-
+df2.rename(columns={"ac_annual":"연간 예측 전력발전량(kWh)"},inplace = True)
 px.set_mapbox_access_token(open("mapbox_token.py").read()) # you will need your own token
 
 
 
-fig = px.scatter_mapbox(df2, lat="lat", lon="lng", color='ac_annual',hover_name="주소",hover_data=["ac_annual"],
+fig = px.scatter_mapbox(df2, lat="lat", lon="lng", color='연간 예측 전력발전량(kWh)',hover_name="주소",hover_data=["연간 예측 전력발전량(kWh)"],
                         color_continuous_scale=px.colors.sequential.Viridis,zoom=7
                        )
 
